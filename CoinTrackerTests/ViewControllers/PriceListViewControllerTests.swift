f3be81da299fb31155cc23582ea0d3cd7b9f21ab
Florin Uscatu <>
2019-12-16 16:32:02 +0200

    Add price list tests

--- CoinTrackerTests/ViewControllers/PriceListViewControllerTests.swift
+++ CoinTrackerTests/ViewControllers/PriceListViewControllerTests.swift
@@ -27,12 +27,20 @@ class PriceListViewControllerTests: XCTestCase {
         XCTAssertEqual(viewController.tableView(viewController.tableView, numberOfRowsInSection: 0), 8)
     }
     
-    func test_viewDidLoad_rendersPriceAndDateInCells_isCorrect() {
+    func test_viewDidLoad_rendersPriceAndDateInHistoricalPriceCells_isCorrect() {
         let viewController = makeSUT()
         let fourthCell = viewController.tableView(viewController.tableView, cellForRowAt: IndexPath(row: 3, section: 0)) as? ListCell
 
         XCTAssertEqual(fourthCell?.mainLabel.text, "6,119.38 EUR")
         XCTAssertEqual(fourthCell?.secondaryLabel.text, "2019-11-27")
+    }
+    
+    func test_viewDidLoad_rendersPriceAndDateInCurrentPriceCell_isCorrect() {
+        let viewController = makeSUT()
+        let fourthCell = viewController.tableView(viewController.tableView, cellForRowAt: IndexPath(row: 0, section: 0)) as? ListCell
+
+        XCTAssertEqual(fourthCell?.mainLabel.text, "6,549.35 EUR")
+        XCTAssertEqual(fourthCell?.secondaryLabel.text, "Today")
     }
     
     // MARK: - Helpers
